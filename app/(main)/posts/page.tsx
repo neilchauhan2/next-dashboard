088d2c979b284e371f39d5757ca8dcfa384fab04
Neil Chauhan <neilchauhan2@gmail.com>
2024-06-05 19:01:52 +0530

    feat: Add back button and pagination component ui

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,9 +1,13 @@
+import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/Posts/PostsPagination";
 import PostsTable from "@/components/Posts/PostsTable";
 
 function PostsPage() {
   return (
     <>
+      <BackButton text="Go Back" link="/" />
       <PostsTable />
+      <PostsPagination />
     </>
   )
 }
