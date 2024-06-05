59ca164f0567dd29b3ba5d8c3db320180520a6a8
Neil Chauhan <neilchauhan2@gmail.com>
2024-06-05 11:49:27 +0530

    feat: Add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,40 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import Link from "next/link"
+
+function Navbar() {
+  return (
+    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between">
+      <Link href='/' className="text-2xl">Acme Logo</Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger className='focus:outline-none'>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-black">NC</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href='/profile'>Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href='/auth'>Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+    </div>
+  )
+}
+
+export default Navbar
